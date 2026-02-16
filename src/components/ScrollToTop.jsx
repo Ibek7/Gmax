bde4fde1e35ee10909648ae95d681e7ef3fcad57
Bekam Guta <bekamdawit551@gmail.com>
2026-02-16 11:38:34 -0500

    feat: create ScrollToTop component

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import '../styles/ScrollToTop.css'
+
+const ScrollToTop = () => {
+    const [isVisible, setIsVisible] = useState(false)
+
+    // Show button when page is scrolled up to given distance
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setIsVisible(true)
+        } else {
+            setIsVisible(false)
+        }
+    }
+
+    // Set the top cordinate to 0
+    // make scrolling smooth
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility)
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility)
+        }
+    }, [])
+
+    return (
+        <>
+            {isVisible && (
+                <div onClick={scrollToTop} className="scroll-to-top" aria-label="Scroll to top" title="Scroll to top">
+                    ↑
+                </div>
+            )}
+        </>
+    )
+}
+
+export default ScrollToTop
