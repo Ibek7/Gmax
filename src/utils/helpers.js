709f159be9e0ef08bc1c442a63f61bfd413140af
Bekam Guta <bekamdawit551@gmail.com>
2026-02-18 09:42:55 -0500

    feat: add debounce utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27,6 +27,18 @@ export const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+export const debounce = (func, wait) => {
+  let timeout
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout)
+      func(...args)
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  }
+}
+
 export const formatDuration = (ms) => {
   const minutes = Math.floor(ms / 60000);
   const seconds = ((ms % 60000) / 1000).toFixed(0);
